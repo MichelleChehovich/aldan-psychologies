3c04fdcd554fc66de5e18b5477af433f09561184
MichelleChehovich <michellechehovich@gmail.com>
2026-04-25 23:47:44 +0700

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,11 +1,31 @@
-from pydantic import BaseModel
+#from pydantic import BaseModel
 
+from datetime import datetime
+from pydantic import BaseModel
 
 class PsychologistCreate(BaseModel):
     email: str
     password: str
 
-
 class ClientCreate(BaseModel):
     name: str
+
+class SessionCreate(BaseModel):
+    client_id: int
+    session_date: datetime
+
+class SessionOut(BaseModel):
+    id: int
+    client_id: int
+    session_date: datetime
+    status: str
+
+    class Config:
+        from_attributes = True
+
+class AudioUpdate(BaseModel):
+    audio_url: str
+
+class TranscriptUpdate(BaseModel):
+    transcript: str
 
