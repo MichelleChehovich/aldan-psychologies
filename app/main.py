12dd55c1bdb4530a7425ff66f4ec03e9ae285b49
MichelleChehovich <michellechehovich@gmail.com>
2026-04-26 01:11:45 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,24 +1,46 @@
+print("🔥 MAIN START")
+
 from fastapi import FastAPI
 from .db import Base, engine
 from .routes import router
-from . import models  # 🔥 обязательно!
+from . import models
 
-#print("STEP 1")
+print("🔥 IMPORTS OK")
 
 app = FastAPI()
-
-#print("STEP 2")
 
 @app.get("/")
 def root():
     return {"status": "alive"}
 
-#print("STEP 3")
-
-@app.get("/health")
-def health():
-    return {"status": "ok"}
-
 app.include_router(router)
 
+print("🔥 DB INIT")
+
 Base.metadata.create_all(bind=engine)
+
+
+#from fastapi import FastAPI
+#from .db import Base, engine
+#from .routes import router
+#from . import models  # 🔥 обязательно!
+
+#print("STEP 1")
+
+#app = FastAPI()
+
+#print("STEP 2")
+
+#@app.get("/")
+#def root():
+#    return {"status": "alive"}
+
+#print("STEP 3")
+
+#@app.get("/health")
+#def health():
+#    return {"status": "ok"}
+
+#app.include_router(router)
+
+#Base.metadata.create_all(bind=engine)
