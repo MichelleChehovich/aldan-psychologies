17c149fef29fa89101bcb8955915cbd6a628bfbf
MichelleChehovich <michellechehovich@gmail.com>
2026-04-25 11:32:11 +0700

    Update db.py

--- app/db.py
+++ app/db.py
@@ -3,16 +3,22 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 #DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/app")
+#DATABASE_URL = "sqlite:///./test.db"
 
-DATABASE_URL = "sqlite:///./test.db"
+#engine = create_engine(DATABASE_URL)
+#SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#Base = declarative_base()
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+#def get_db():
+#    db = SessionLocal()
+#    try:
+#        yield db
+#    finally:
+#        db.close()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}  # ВАЖНО для SQLite
+)
