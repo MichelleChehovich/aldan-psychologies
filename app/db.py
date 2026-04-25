58215e5bad465e58965399a26c59e02d3a9b2e2a
MichelleChehovich <michellechehovich@gmail.com>
2026-04-26 01:27:05 +0700

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,4 +1,5 @@
-from sqlalchemy.orm import sessionmaker
+#from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from .models import Base
 from sqlalchemy import create_engine
 import os
@@ -19,7 +20,8 @@ def get_db():
         yield db
     finally:
         db.close()
-
+        
+Base = declarative_base()
 
 #from sqlalchemy import create_engine
 ###                                                                     from sqlalchemy.orm import sessionmaker, declarative_base
