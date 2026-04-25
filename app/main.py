b61ffd2799789d4b35a8111dc93265c4ae13b1d3
MichelleChehovich <michellechehovich@gmail.com>
2026-04-25 12:07:51 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,17 +2,16 @@ from fastapi import FastAPI
 from .db import Base, engine
 from .routes import router
 
-Base.metadata.create_all(bind=engine)
-
-#app = FastAPI()
-app = FastAPI(
-    docs_url="/docs",
-    redoc_url="/redoc",
-    openapi_url="/openapi.json"
-)
+app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "API is running"}
 
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
 app.include_router(router)
+
+Base.metadata.create_all(bind=engine)
