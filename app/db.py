193c8eaa83add88897edba8108702c999fb19f24
MichelleChehovich <michellechehovich@gmail.com>
2026-04-26 00:23:30 +0700

    Update db.py

--- app/db.py
+++ app/db.py
@@ -6,6 +6,10 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}  
+    connect_args={"check_same_thread": False}
 )
-# ВАЖНО для SQLite
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# 🔥 ВОТ ЭТОЙ СТРОКИ У ТЕБЯ СКОРЕЕ ВСЕГО НЕТ
+Base = declarative_base()
