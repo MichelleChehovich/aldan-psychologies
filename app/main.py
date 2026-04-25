8b96422da34332c91aaf325eec3304d6125f8a46
MichelleChehovich <michellechehovich@gmail.com>
2026-04-26 00:17:50 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from .db import Base, engine
+from .routes import router
+from . import models  # 🔥 обязательно!
 
 #print("STEP 1")
 
@@ -15,5 +18,7 @@ def root():
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+app.include_router(router)
 
 Base.metadata.create_all(bind=engine)
