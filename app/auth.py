43b230f3b526b3dd85a3ca5caf9e27675373206c
MichelleChehovich <michellechehovich@gmail.com>
2026-04-28 21:12:34 +0700

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -6,21 +6,18 @@ SECRET_KEY = "supersecret"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
+
 
 def hash_password(password: str):
-    return pwd_context.hash(password)
+    return pwd_context.hash(password[:72])
+
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
-
-#
-#def hash_password(password: str):
-#    return pwd_context.hash(password)
-
-
-#def verify_password(plain, hashed):
-#    return pwd_context.verify(plain, hashed)
 
 
 def create_access_token(data: dict):
@@ -28,4 +25,3 @@ def create_access_token(data: dict):
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-
