836cca76b68dbc5d6be87ed2a4a7638569d1f310
MichelleChehovich <michellechehovich@gmail.com>
2026-04-28 21:51:20 +0700

    Update deps.py

--- app/deps.py
+++ app/deps.py
@@ -1,20 +1,27 @@
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
-from jose import jwt
+from jose import jwt, JWTError
 from sqlalchemy.orm import Session
 
 from .db import get_db
 from .models import Psychologist
 from .auth import SECRET_KEY, ALGORITHM
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
 
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         user_id = payload.get("sub")
-    except:
+
+        if user_id is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+
+    except JWTError:
         raise HTTPException(status_code=401, detail="Invalid token")
 
     user = db.query(Psychologist).filter(Psychologist.id == int(user_id)).first()
