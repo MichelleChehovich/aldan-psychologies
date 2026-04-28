a74e38dcb04be5fca60f1313311e9be4fc699867
MichelleChehovich <michellechehovich@gmail.com>
2026-04-28 21:14:56 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,23 +1,19 @@
 print("🔥 MAIN START")
 
 from fastapi import FastAPI
-from .models import Base
 from .db import engine
+from .models import Base
 from .routes import router
-
-print("🔥 IMPORTS OK")
 
 app = FastAPI()
 
-
-# ✅ СОЗДАНИЕ ТАБЛИЦ (ключевой момент)
-print("🔥 DB INIT")
+# create tables
 Base.metadata.create_all(bind=engine)
-
 
 @app.get("/")
 def root():
     return {"status": "alive"}
 
-
 app.include_router(router)
+
+print("🔥 APP READY")
