d6ce14f5696259a1a3fafc64f5ec856f2064aa3c
MichelleChehovich <michellechehovich@gmail.com>
2026-05-04 23:32:30 +0700

    Update deps.py

--- app/deps.py
+++ app/deps.py
@@ -2,8 +2,9 @@ from fastapi import Depends, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from supabase import create_client
 
+# ⚠️ Supabase config (должны быть везде одинаковые)
 SUPABASE_URL = "https://gfqafwrjrixglddpiluc.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmcWFmd3Jqcml4Z2xkZHBpbHVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4OTU0NzUsImV4cCI6MjA5MzQ3MTQ3NX0.9hQDdgqyQWVofEzTG4gIPHFELtOsDLU0rISEJVpx3ws"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmcWFmd3JyaXJ4Z2xkZHBpbHVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4OTU0NzUsImV4cCI6MjA5MzQ3MTQ3NX0.9hQDdgqyQWVofEzTG4gIPHFELtOsDLU0rISEJVpx3ws"
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
@@ -12,10 +13,10 @@ security = HTTPBearer()
 
 def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
     token = credentials.credentials
-    
-    user = supabase.auth.get_user(token)
 
-    if not user.user:
+    res = supabase.auth.get_user(token)
+
+    if not res.user:
         raise HTTPException(status_code=401, detail="Invalid token")
 
-    return user.user
+    return res.user
