5620d5b26f5341caad1b1523adb610c221d5a793
MichelleChehovich <michellechehovich@gmail.com>
2026-05-11 22:43:03 +0700

    Refactor app/deps.py for improved readability

--- app/deps.py
+++ app/deps.py
@@ -3,9 +3,6 @@ from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from supabase import create_client
 from .config import SUPABASE_URL, SUPABASE_KEY
 
-#SUPABASE_URL = "https://gfqafwrjrixglddpiluc.supabase.co"
-#SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmcWFmd3Jqcml4Z2xkZHBpbHVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4OTU0NzUsImV4cCI6MjA5MzQ3MTQ3NX0.9hQDdgqyQWVofEzTG4gIPHFELtOsDLU0rISEJVpx3ws"
-
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 security = HTTPBearer()
