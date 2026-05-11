b975d84d1ee6854caaaaaf4e2dfceaea6fdfa4d4
MichelleChehovich <michellechehovich@gmail.com>
2026-05-12 00:05:02 +0700

    Update deps.py

--- app/deps.py
+++ app/deps.py
@@ -1,13 +1,12 @@
+import os  # ← ВОТ ЭТО НУЖНО ДОБАВИТЬ
+
+print("SUPABASE_URL:", os.getenv("SUPABASE_URL"))
+print("SUPABASE_KEY:", os.getenv("SUPABASE_KEY"))
+
 from fastapi import Depends, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from supabase import create_client
 from .config import SUPABASE_URL, SUPABASE_KEY
-import os
-
-print("🔥 DEPS LOADED")
-print("ENV CHECK:")
-print("SUPABASE_URL:", os.getenv("SUPABASE_URL"))
-print("SUPABASE_KEY:", os.getenv("SUPABASE_KEY"))
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
@@ -16,7 +15,6 @@ security = HTTPBearer()
 
 def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
     token = credentials.credentials
-    
     user = supabase.auth.get_user(token)
 
     if not user.user:
