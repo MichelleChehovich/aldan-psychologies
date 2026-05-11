3eb73cc48d11ce8a551c2b286874afce6683d820
MichelleChehovich <michellechehovich@gmail.com>
2026-05-11 20:47:33 +0700

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+#OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+JWT_SECRET = os.getenv("JWT_SECRET")
