2f04d24cba686b3a63890888f4a086f9bc33953f
MichelleChehovich <michellechehovich@gmail.com>
2026-05-11 21:39:53 +0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,11 +1,17 @@
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
+#import os
 import os
-
-load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
+
+
+#load_dotenv()
+
+#SUPABASE_URL = os.getenv("SUPABASE_URL")
+#SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
 #OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-JWT_SECRET = os.getenv("JWT_SECRET")
+#JWT_SECRET = os.getenv("JWT_SECRET")
