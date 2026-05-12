7e5207c58bd727a442fb7864221d9fa03cd3d863
MichelleChehovich <michellechehovich@gmail.com>
2026-05-13 02:57:36 +0700

    Update schemas.py

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 from typing import Optional
 
 
@@ -13,7 +13,7 @@ class PsychologistCreate(BaseModel):
 
 class PsychologistOut(BaseModel):
     id: str
-    email: EmailStr
+    email: str
     created_at: datetime
     role: str
 
