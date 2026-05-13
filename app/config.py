28f43f9d6d4fa69cc7e5f553bc0b2b7904be88bd
MichelleChehovich <michellechehovich@gmail.com>
2026-05-14 00:14:36 +0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,20 @@
+#import os
+
+#SUPABASE_URL = os.getenv("SUPABASE_URL")  
+#SUPABASE_KEY = os.getenv("SUPABASE_KEY")  
+
 import os
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")  
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")  
+
+def get_supabase_url():
+    url = os.getenv("SUPABASE_URL")
+    if not url:
+        raise RuntimeError("SUPABASE_URL is missing")
+    return url
+
+
+def get_supabase_key():
+    key = os.getenv("SUPABASE_KEY")
+    if not key:
+        raise RuntimeError("SUPABASE_KEY is missing")
+    return key
