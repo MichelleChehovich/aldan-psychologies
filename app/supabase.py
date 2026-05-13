391a7db6ba88f7287fb4ac297c92d5343295e715
MichelleChehovich <michellechehovich@gmail.com>
2026-05-14 00:12:52 +0700

    Update supabase.py

--- app/supabase.py
+++ app/supabase.py
@@ -1,8 +1,29 @@
-from supabase import create_client
-from app.config import SUPABASE_URL, SUPABASE_KEY
+#from supabase import create_client
+#from app.config import SUPABASE_URL, SUPABASE_KEY
 
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+#supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+#def get_supabase():
+#    return supabase
+
+import os
+from supabase import create_client
+
+_supabase_client = None
 
 
 def get_supabase():
-    return supabase
+    global _supabase_client
+
+    if _supabase_client is not None:
+        return _supabase_client
+
+    url = os.getenv("SUPABASE_URL")
+    key = os.getenv("SUPABASE_KEY")
+
+    if not url or not key:
+        raise RuntimeError("SUPABASE_URL or SUPABASE_KEY is missing")
+
+    _supabase_client = create_client(url, key)
+
+    return _supabase_client
