eed3828c593c31cae06e89af96ed8fd58971272f
MichelleChehovich <michellechehovich@gmail.com>
2026-05-13 23:13:29 +0700

    Update supabase.py

--- app/supabase.py
+++ app/supabase.py
@@ -1,6 +1,8 @@
 from supabase import create_client
 from app.config import SUPABASE_URL, SUPABASE_KEY
 
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
 
 def get_supabase():
-    return create_client(SUPABASE_URL, SUPABASE_KEY)
+    return supabase
